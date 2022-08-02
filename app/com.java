035e64e186a777851357e18e44a75982bc1d4a91
PSH64 <qkrtjdgustjdgus@naver.com>
2022-08-02 14:08:39 +0900

    Initial commit

---
+++ app/com.java
@@ -0,0 +1,4 @@
+# Add any project specific keep options here:
+-keep class com.google.android.gms.location.** { *; }
+-keep class com.google.android.gms.common.** { *; }
+-keep class com.google.android.gms.tasks.** { *; }
